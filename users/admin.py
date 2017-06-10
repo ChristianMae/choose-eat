afc612606cc5d08e9d087aa852e2226de6a74982
sbenemerito <benemeritosam@gmail.com>
2017-06-10 15:55:19 +0800

    initial models, no prefs

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Profile
+from .models import UserGroup, Profile, Friendship
 
 # Register your models here.
+admin.site.register(UserGroup)
 admin.site.register(Profile)
+admin.site.register(Friendship)
--- users/models.py
+++ users/models.py
@@ -4,10 +4,27 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
+class UserGroup(models.Model):
+	"""
+	Group model for group recommendations
+	A user can create groups, name it, and add other users to it.
+	"""
+	name = models.CharField(max_length=50, blank=True, default='Unnamed Group')
+	creator = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.name
+
+
 class Profile(models.Model):
+	"""
+	Extends User Model, adding bio, birthdate, and groups 
+	a user belongs to.
+	"""
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	bio = models.TextField(max_length=500, blank=True)
 	birth_date = models.DateField(null=True, blank=True)
+	groups = models.ManyToManyField(UserGroup)
 
 
 @receiver(post_save, sender=User)
@@ -19,3 +36,13 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+
+class Friendship(models.Model):
+	"""
+	Relationship of UserA and UserB (for Friend List)
+	Status = 0 (Pending request) | 1 (Accepted)
+	"""
+	user_a = models.OneToOneField(User, on_delete=models.CASCADE, related_name='userA')
+	user_b = models.OneToOneField(User, on_delete=models.CASCADE, related_name='userB')
+	friends = models.BooleanField(default=False)
