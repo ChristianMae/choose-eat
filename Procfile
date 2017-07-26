009a12a115b37dee059f66b5265eab2ab1082f57
sbenemerito <benemeritosam@gmail.com>
2017-07-26 23:52:02 +0800

    added files for initial deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ceat.wsgi --log-file -
--- ceat/settings.py
+++ ceat/settings.py
@@ -24,9 +24,9 @@ with open(os.path.join(BASE_DIR, 'secretkey.txt')) as f:
     SECRET_KEY = f.read()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 AUTH_USER_MODEL = 'users.User'
 
 # Application definition
