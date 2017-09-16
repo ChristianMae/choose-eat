fbda466f52b60d8c67e211ca95f019f19c98af52
sbenemerito <benemeritosam@gmail.com>
2017-09-16 22:18:22 +0800

    fixed recommendation bug, updated user serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,51 @@
 from rest_framework import serializers
-from .models import User
+from .models import User, History
+import json
+
+class UserDetailsSerializer(serializers.ModelSerializer):
+    history = serializers.SerializerMethodField()
+
+    def get_history(self, obj):
+        try:
+            myUser = User.objects.get(id=obj.id)
+            userHistory = History.objects.filter(user=myUser)
+            historyToReturn = []
+            for history in userHistory:
+                if history.isLiked in (True, False):
+                    continue
+                newHistory = dict()
+                newHistory['id'] = history.id
+                newHistory['resto'] = history.resto
+                newHistory['categories'] = history.categories
+                newHistory['isLiked'] = history.isLiked
+                newHistory['date_went'] = str(history.date_went)
+                historyToReturn.append(newHistory)
+            return json.dumps(historyToReturn)
+        except Exception as e:
+            return str(e)
+
+    ratedhistory = serializers.SerializerMethodField()
+
+    def get_ratedhistory(self, obj):
+        try:
+            myUser = User.objects.get(id=obj.id)
+            userHistory = History.objects.filter(user=myUser)
+            historyToReturn = []
+            for history in userHistory:
+                if history.isLiked not in (True, False):
+                    continue
+                newHistory = dict()
+                newHistory['id'] = history.id
+                newHistory['resto'] = history.resto
+                newHistory['categories'] = history.categories
+                newHistory['isLiked'] = history.isLiked
+                newHistory['date_went'] = str(history.date_went)
+                historyToReturn.append(newHistory)
+            return json.dumps(historyToReturn)
+        except Exception as e:
+            return str(e)
 
 class UserDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'bio', 'birth_date', 'preferences', 'avatar_url', 'date_joined')
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'bio', 'birth_date', 'preferences', 'avatar_url', 'date_joined', 'history', 'ratedhistory')
